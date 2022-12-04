3f01f00f17ea4ca34b011835fef9ae36990d19b0
Emmanuel Guides <eguides@up.edu.ph>
2022-12-04 14:00:15 +0800

    bind widgets

--- testing_tech/android-wifidirect/app/src/main/java/com/example/androidwifidirect_test/MainActivity.java
+++ testing_tech/android-wifidirect/app/src/main/java/com/example/androidwifidirect_test/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.androidwifidirect_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnOnOff, btnDiscover, btnSend;
+    ListView listView;
+    TextView read_msg_box, connectionStatus;
+    EditText writeMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initEverything();
+    }
+
+    public void initEverything(){
+        btnOnOff = (Button) findViewById(R.id.onOff);
+        btnDiscover = (Button) findViewById(R.id.discover);
+        btnSend = (Button) findViewById(R.id.sendButton);
+        listView = (ListView) findViewById(R.id.peerListView);
+        read_msg_box = (TextView) findViewById(R.id.readMsg);
+        connectionStatus = (TextView) findViewById(R.id.connectionStatus);
+        writeMsg = (EditText) findViewById(R.id.writeMsg);
     }
 }
